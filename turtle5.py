8eced995a5734035528192e141b6ce6ac1b8d060
itc-25002 <s25002@std.it-college.ac.jp~>
2025-06-09 17:02:35 +0900

    250609

--- turtle5.py
+++ turtle5.py
@@ -1,5 +1,9 @@
 from turtle import *
 shape("turtle")
+color('orange')
+speed(10)
 for i in range(50):
     circle(i * 2)
+    left(25)
+hideturtle()
 done()
---
+++ turtle6.py
@@ -0,0 +1,9 @@
+from turtle import *
+shape("turtle")
+color('orange')
+speed(10)
+for i in range(24):
+    circle(45)
+    left(15)
+hideturtle()
+done()

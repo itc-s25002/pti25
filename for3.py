995b793a52e08901c61861188fdd5c362b475630
itc-s25002 <s25002@std.it-college.ac.jp>
2025-06-16 07:56:19 +0000

    250616

---
+++ for3.py
@@ -0,0 +1,6 @@
+total = 0
+scorelist = [64,100,78,80,72]
+for i in scorelist:
+    total = total + i
+
+print(total)
---
+++ if2.py
@@ -0,0 +1,6 @@
+score = 60
+if score >= 80:
+    print("やったね！")
+    print("次もその調子だ")
+else:
+    print("残念でした")
